675ecc4f2fa00fd5bd03c9f0282bf62bb57f5de6
Vsyatkin <vsyatkin@airtaero.com>
2024-07-24 21:01:57 +0700

    add_build

--- src/typings.d.ts
+++
@@ -1,7 +0,0 @@
-declare module '*.module.css';
-declare module '*.module.scss';
-
-declare module '*.svg' {
-  const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
-  export default content;
-}
